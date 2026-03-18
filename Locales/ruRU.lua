211b08e3d182b95387fa28e3aaf301546bb37598
Azro <47503034+LoyalFTW@users.noreply.github.com>
2026-03-18 01:29:32 -0300

    Update locale initialization in ruRU.lua

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -1,4 +1,4 @@
-﻿local L = LibStub("AceLocale-3.0"):NewLocale("MidnightRoutine", "ruRU", true)
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("MidnightRoutine", "ruRU")
 if not L then return end
 
 -- Core / Chat
