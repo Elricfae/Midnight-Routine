ab58c7735f2074da3bd26a6c7d2b952456e996df
Azro <47503034+LoyalFTW@users.noreply.github.com>
2026-03-18 13:38:48 -0300

    Crests
    * removed the need of translations for the keys we are now going to test using the API to get the names
    * will keep the translations for now incase this doesnt work well

--- Modules/Crests.lua
+++ Modules/Crests.lua
@@ -1,9 +1,15 @@
 local CREST_CAP = 100
 local L = LibStub("AceLocale-3.0"):GetLocale("MidnightRoutine")
 
-local function ItemLabel(itemID, fallback)
+local function ItemLabel(itemID, fallback, color)
     local itemName = C_Item and C_Item.GetItemNameByID and C_Item.GetItemNameByID(itemID)
-    return string.format("|cffe8c96e%s:|r", itemName or fallback)
+    return string.format("|cff%s%s:|r", color or "e8c96e", itemName or fallback)
+end
+
+local function CurrencyLabel(currencyID, fallback, color)
+    local info = C_CurrencyInfo and C_CurrencyInfo.GetCurrencyInfo and C_CurrencyInfo.GetCurrencyInfo(currencyID)
+    local currencyName = info and info.name
+    return string.format("|cff%s%s:|r", color or "e8c96e", currencyName or fallback)
 end
 
 MR:RegisterModule({
@@ -13,42 +19,77 @@ MR:RegisterModule({
     resetType   = "weekly",
     defaultOpen = true,
     rows = {
-        { key = "crest_adventurer", currencyId = 3383, max = CREST_CAP, label = L["Crest_Adventurer_Label"] },
-        { key = "crest_veteran",    currencyId = 3341, max = CREST_CAP, label = L["Crest_Veteran_Label"] },
-        { key = "crest_champion",   currencyId = 3343, max = CREST_CAP, label = L["Crest_Champion_Label"] },
-        { key = "crest_hero",       currencyId = 3345, max = CREST_CAP, label = L["Crest_Hero_Label"] },
-        { key = "crest_myth",       currencyId = 3347, max = CREST_CAP, label = L["Crest_Myth_Label"] },
-        { key = "manaflux",         currencyId = 3378, noMax = true, label = L["Currency_Manaflux_Label"] },
-        { key = "voidlight_marl",   currencyId = 3316, noMax = true, label = L["Currency_VoidlightMarl_Label"] },
         {
-            key        = "shards",
-            label      = L["CofferKey_Label"],
+            key = "crest_adventurer",
+            currencyId = 3383,
+            max = CREST_CAP,
+            label = CurrencyLabel(3383, nil, "b7b7b7"),
+        },
+        {
+            key = "crest_veteran",
+            currencyId = 3341,
+            max = CREST_CAP,
+            label = CurrencyLabel(3341, nil, "1eff00"),
+        },
+        {
+            key = "crest_champion",
+            currencyId = 3343,
+            max = CREST_CAP,
+            label = CurrencyLabel(3343, nil, "f1c232"),
+        },
+        {
+            key = "crest_hero",
+            currencyId = 3345,
+            max = CREST_CAP,
+            label = CurrencyLabel(3345, nil, "0070dd"),
+        },
+        {
+            key = "crest_myth",
+            currencyId = 3347,
+            max = CREST_CAP,
+            label = CurrencyLabel(3347, nil, "ff8000"),
+        },
+        {
+            key = "manaflux",
+            currencyId = 3378,
+            noMax = true,
+            label = CurrencyLabel(3378, nil),
+        },
+        {
+            key = "voidlight_marl",
+            currencyId = 3316,
+            noMax = true,
+            label = CurrencyLabel(3316, nil),
+        },
+        {
+            key = "shards",
             currencyId = 3310,
-            max        = 600,
+            max = 600,
+            label = CurrencyLabel(3310, nil),
         },
         {
-            key        = "restored_coffer_key",
-            label      = L["Currency_RestoredCofferKey_Label"],
+            key = "restored_coffer_key",
             currencyId = 3028,
-            noMax      = true,
+            noMax = true,
+            label = CurrencyLabel(3028),
         },
         {
-            key    = "spark_radiance",
-            label  = ItemLabel(232875, "Spark of Radiance"),
+            key = "spark_radiance",
             itemId = 232875,
-            noMax  = true,
+            noMax = true,
+            label = ItemLabel(232875, nil),
         },
         {
-            key        = "undercoin",
-            label      = L["Currency_Undercoin_Label"],
+            key = "undercoin",
             currencyId = 2803,
-            noMax      = true,
+            noMax = true,
+            label = CurrencyLabel(2803, nil),
         },
         {
-            key        = "shard_dundun",
-            label      = L["Shard_Dundun_Label"],
+            key = "shard_dundun",
             currencyId = 3376,
-            noMax      = true,
+            noMax = true,
+            label = CurrencyLabel(3376, nil),
         },
     },
 })
